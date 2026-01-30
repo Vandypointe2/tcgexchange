f5b2d3b27b8f2eb00c628d5df43d816141e8afd3
Kyle <125912717+Vandypointe2@users.noreply.github.com>
2026-01-29 22:49:02 -0600

    Add profile page (username + avatar URL) and change password

---
+++ migrations/20260130050000-add-user-profile-fields.js
@@ -0,0 +1,12 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'avatarUrl', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn('Users', 'avatarUrl');
+  }
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -8,5 +8,7 @@ router.post('/signup', usersController.signup);
 router.post('/login', usersController.login);
 router.get('/validate', authMiddleware, usersController.validate);
 router.get('/me', authMiddleware, usersController.getCurrentUser);
+router.put('/me', authMiddleware, usersController.updateProfile);
+router.post('/change-password', authMiddleware, usersController.changePassword);
 
 module.exports = router;
